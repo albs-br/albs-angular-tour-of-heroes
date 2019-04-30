e2ecafd51ee209831716759ffb893d93f38a06be
albs-br <albs_br@yahoo.com.br>
2019-04-30 14:41:57 -0300

    v.0.0.3 4 files modified, 1 added

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-heroes',
@@ -8,7 +9,10 @@ import { Component, OnInit } from '@angular/core';
 
 export class HeroesComponent implements OnInit {
 
-  hero = 'Windstorm';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
 
   constructor() { }
 
