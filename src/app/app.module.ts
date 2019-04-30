15164354e599220e73f4952075b109818fb9220d
albs-br <albs_br@yahoo.com.br>
2019-04-30 14:52:56 -0300

    v.0.0.4 4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+// import { HelloComponent } from './hello.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, HeroesComponent],
+  declarations: [
+    AppComponent, 
+    // HelloComponent, 
+    HeroesComponent
+    ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,10 +1,10 @@
-import { Component, Input } from '@angular/core';
+// import { Component, Input } from '@angular/core';
 
-@Component({
-  selector: 'hello',
-  template: `<h1>Hello {{name}}!</h1>`,
-  styles: [`h1 { font-family: Lato; }`]
-})
-export class HelloComponent  {
-  @Input() name: string;
-}
+// @Component({
+//   selector: 'hello',
+//   template: `<h1>Hello {{name}}!</h1>`,
+//   styles: [`h1 { font-family: Lato; }`]
+// })
+// export class HelloComponent  {
+//   @Input() name: string;
+// }
