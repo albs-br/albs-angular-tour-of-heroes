a5e3150f786ae57bad2baa2b477f00893e49a0ba
albs-br <albs_br@yahoo.com.br>
2019-04-30 10:37:36 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# albs-angular-tour-of-heroes
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/albs-angular-tour-of-heroes)
---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { HeroesComponent } from './heroes/heroes.component';
+
+@NgModule({
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HelloComponent, HeroesComponent],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
