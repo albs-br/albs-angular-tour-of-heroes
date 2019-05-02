f25df2c37e4bf78995fe212acf838cb3ec218679
albs-br <albs_br@yahoo.com.br>
2019-05-02 12:00:37 -0300

    v.0.0.12 4 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,14 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 // import { HeroService } from './hero.service';
 // import { MessageService } from './message.service';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+    ],
   declarations: [
     AppComponent, 
     // HelloComponent, 
